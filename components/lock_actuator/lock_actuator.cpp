4322fdea7ec80a12abedb42c49072b0b60608b23
FeMtzo <felipe_pm2004@hotmail.com>
2025-09-09 13:18:19 -0300

    Teste: terça feira 09/09

--- components/lock_actuator/lock_actuator.cpp
+++ components/lock_actuator/lock_actuator.cpp
@@ -4,6 +4,8 @@
 #include "esp_log.h"
 #include "ui_feedback.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 // Implementação da função
 void lock_actuator_init(void)
 {
@@ -21,12 +23,18 @@ void lock_actuator_open(void)
     ESP_LOGI(TAG_LOCK, "Abrindo a trava...");
     // Ativa o atuador (assumindo que HIGH abre a trava)
     gpio_set_level((gpio_num_t)LOCK_RELAY_PIN, 1);
+    gpio_set_level((gpio_num_t)LED_GREEN_PIN, 1); // Acende o LED indicador
+
 }
 
 void lock_actuator_close(void)
 {
     ESP_LOGI(TAG_LOCK, "Fechando a trava...");
     gpio_set_level((gpio_num_t)LOCK_RELAY_PIN, 0);
+    gpio_set_level((gpio_num_t)LED_GREEN_PIN, 0); // Apaga o LED indicador
+    gpio_set_level((gpio_num_t)LED_RED_PIN, 1);   // Acende o LED vermelho ao fechar
+    vTaskDelay(pdMS_TO_TICKS(500));                // Mantém o LED vermelho
+   // Apaga o LED vermelho
     
 }
 
